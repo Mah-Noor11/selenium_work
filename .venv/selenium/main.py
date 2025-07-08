2e7ab2c4372fe8796a08863809612c91755ad266
mah noor <mahnoortech738@gmail.com>
2025-07-09 00:47:23 +0500

    Initial commit

---
+++ .venv/selenium/main.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time 
+# This script uses selenium to automate a web browser.
+
+driver = webdriver.Chrome()
+driver.get("http://www.python.org")
+assert "Python" in driver.title
+elem = driver.find_element(By.NAME, "q")
+elem.clear()
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+assert "No results found." not in driver.page_source
+time.sleep(8)  # wait for 5 seconds to see the results
+driver.close()
